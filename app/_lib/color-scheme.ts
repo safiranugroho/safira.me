9a03710c0ad4ecfbbc5318594b07a19c5b47a6fe
Safira Nugroho <safiranugroho@gmail.com>
2023-06-18 23:02:19 +1000

    remove ocean-ruby for now

--- app/_lib/color-scheme.ts
+++ app/_lib/color-scheme.ts
@@ -47,11 +47,6 @@ const colorSchemes: Record<string, ColorScheme> = {
     theme: 'prose-peach-army',
     bgLight: 'bg-peach-500',
     bgDark: 'dark:bg-army-500'
-  },
-  'ocean-ruby': {
-    theme: 'prose-ocean-ruby',
-    bgLight: 'bg-ocean-500',
-    bgDark: 'dark:bg-ruby-500'
   }
 };
 
