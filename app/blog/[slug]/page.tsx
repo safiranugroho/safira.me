c7f6afd876ddb94d80bd0056f71112ebb853eeaa
Safira Nugroho <safiranugroho@gmail.com>
2023-06-18 14:00:39 +1000

    use gh-flavored markdown

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { compileMDX } from 'next-mdx-remote/rsc';
+import remarkGfm from 'remark-gfm';
 import Layout from "./layout";
 
 type Frontmatter = {
@@ -22,7 +23,9 @@ export default async function Page({ params }: PageProps) {
   const { slug } = params;
   const dirPath = path.join(process.cwd(), "mdx");
   const source = fs.readFileSync(`${dirPath}/${slug}.mdx`, "utf-8");
-  const { content, frontmatter } = await compileMDX<Frontmatter>({ source, options: { parseFrontmatter: true }})
+  const { content, frontmatter } = await compileMDX<Frontmatter>({ source, options: { mdxOptions: {
+    remarkPlugins: [remarkGfm]
+  }, parseFrontmatter: true }})
 
   return (
     <Layout>
