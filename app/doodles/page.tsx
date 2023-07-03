54269a2a0bfb8fd6d6d016174622e3af2f3a167f
Safira Nugroho <safiranugroho@gmail.com>
2023-07-03 18:02:08 +1000

    add doodles list

--- app/doodles/page.tsx
+++ app/doodles/page.tsx
@@ -3,9 +3,13 @@ import { doodles } from "./_doodles";
 
 export default async function Page() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col md:max-w-prose">
+      <h1>Doodles</h1>
       {doodles.map((d, i) => (
-        <Link key={i} href={`/doodles/${d.slug}`}>{d.title}</Link>
+        <div key={i} className="mb-8">
+          <Link href={`/doodles/${d.slug}`} className="text-2xl">{d.title}</Link>
+          <p className="m-0">{d.description}</p>
+        </div>
       ))}
     </div>
   );
