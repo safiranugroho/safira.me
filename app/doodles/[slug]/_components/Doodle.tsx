544d6f6332b70bc89bb792d3c47bd2d641f755f4
Safira Nugroho <safiranugroho@gmail.com>
2023-07-03 18:35:39 +1000

    some doodles are desktop only sry

--- app/doodles/[slug]/_components/Doodle.tsx
+++ app/doodles/[slug]/_components/Doodle.tsx
@@ -2,14 +2,25 @@
 
 import '@safiranugroho/doodles/components';
 import '@safiranugroho/doodles/fonts';
+import { doodles } from '../../_doodles';
 
 type DoodleProps = {
   name: string;
 }
 
 export function Doodle({ name }: DoodleProps) {
+  const doodle = doodles.find(d => d.slug === name)!;
+  const isAvailableOnMobile = doodle.screen.includes('mobile') || doodle.screen.includes('all');
+  const isAvailableOnTablet = doodle.screen.includes('tablet') || doodle.screen.includes('all');
+
+  if (window.screen.width < 600 && !isAvailableOnMobile) {
+    return <p>This doodle is only available on tablet and desktop, sorry!</p>;
+  }
+
+  if (window.screen.width >= 600 && window.screen.width < 960 && !isAvailableOnTablet) {
+    return <p>This doodle is only available on desktop, sorry!</p>;
+  }
+
   const Component = `doodle-${name}`;
-  return <>
-    <Component></Component>
-  </>
+  return <Component></Component>;
 }
