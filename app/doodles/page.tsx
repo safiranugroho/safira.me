c509d237f8761d7715cdc5de2b0ae163ab65a521
Safira Nugroho <safiranugroho@gmail.com>
2023-07-09 14:06:24 +0700

    import doodles metadata

--- app/doodles/page.tsx
+++ app/doodles/page.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
-import { doodles } from "./_doodles";
+import { doodles } from "@safiranugroho/doodles/metadata";
+
+type Doodle = { 
+  slug: string; 
+  title: string, 
+  description: string 
+};
 
 export default async function Page() {
   return (
     <div className="flex flex-col md:max-w-prose">
       <h1>Doodles</h1>
-      {doodles.map((d, i) => (
+      {doodles.map((d: Doodle, i: number) => (
         <div key={i} className="mb-8">
           <Link href={`/doodles/${d.slug}`} className="text-2xl">{d.title}</Link>
           <p className="m-0">{d.description}</p>
---
+++ doodles.d.ts
@@ -0,0 +1,1 @@
+declare module '@safiranugroho/doodles/metadata'
