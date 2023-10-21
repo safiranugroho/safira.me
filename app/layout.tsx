f4f7c02d9cf49d8cac10f52c5dbf9a2b666dfd44
Safira Nugroho <safiranugroho@gmail.com>
2023-10-21 20:56:55 +1100

    add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Metadata } from 'next'
 import { Navigation } from './_components/Navigation'
+import Script from 'next/script'
 
 type RootLayoutProps = {
   children: React.ReactNode
@@ -11,6 +12,24 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
   return (
     <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-PYH8MSDG00"></Script>
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PYH8MSDG00');
+
+          let colorScheme = sessionStorage.getItem('color-scheme');
+          let theme = localStorage.getItem('theme');
+
+          gtag('event', 'screen_view', {
+            'color_scheme': colorScheme,
+            'theme': theme
+          });
+        `}
+      </Script>
       <body className={`relative bg-none max-w-full flex flex-col md:flex-row ${prose}`}>
         <Navigation />
         <main className="flex w-full flex-col p-8 md:py-12">
