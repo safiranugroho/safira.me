f279285a0f147147a4692c6e3a6f655126bf59f1
Safira Nugroho <safiranugroho@gmail.com>
2023-10-23 03:10:20 +1100

    style: hide non-responsive doodles hehe

--- app/doodles/[slug]/_components/Doodle.tsx
+++ app/doodles/[slug]/_components/Doodle.tsx
@@ -4,6 +4,7 @@ import '@safiranugroho/doodles/components';
 import '@safiranugroho/doodles/fonts';
 import { doodles } from '@safiranugroho/doodles/metadata';
 import { Doodle } from '../../types';
+import { useCallback, useEffect, useState } from 'react';
 
 type DoodleProps = {
   name: string;
@@ -14,12 +15,26 @@ export function Doodle({ name }: DoodleProps) {
   const isAvailableOnMobile = doodle.screen.includes('mobile') || doodle.screen.includes('all');
   const isAvailableOnTablet = doodle.screen.includes('tablet') || doodle.screen.includes('all');
 
-  if (window.screen.width < 600 && !isAvailableOnMobile) {
-    return <p>This doodle is only available on tablet and desktop, sorry!</p>;
+  const [screenWidth, setScreenWidth] = useState(window.screen.width);
+  const handleWindowResize = useCallback(() => setScreenWidth(window.innerWidth), []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+        window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [handleWindowResize]);
+
+  if (screenWidth >= 600 && screenWidth < 960 && !isAvailableOnTablet) {
+    return <p>This doodle is only available on desktop, sorry!</p>;
   }
 
-  if (window.screen.width >= 600 && window.screen.width < 960 && !isAvailableOnTablet) {
+  if (screenWidth < 600 && !isAvailableOnMobile && !isAvailableOnTablet) {
     return <p>This doodle is only available on desktop, sorry!</p>;
+  }
+
+  if (screenWidth < 600 && !isAvailableOnMobile) {
+    return <p>This doodle is only available on tablet and desktop, sorry!</p>;
   }
 
   const Component = `doodle-${name}`;
